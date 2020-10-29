f3c54b3ca35b40426a379d243fb2fc3347bb09e9
mahanmmi <mahanmmi@gmail.com>
2020-10-29 16:32:09 +0330

    GUI framework added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div style="height: 100vh">
+<!--        <div id="app">-->
+<!--            <img alt="Vue logo" src="./assets/logo.png">-->
+<!--            <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+<!--        </div>-->
+        <section style="height: 100%">
+            <ChatPanel style="height: 100%;"/>
+        </section>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    //import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import ChatPanel from "./components/ChatPanel";
+    export default {
+        name: 'App',
+        components: {
+            ChatPanel,
+            //HelloWorld
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ client/src/components/ChatPanel.vue
@@ -0,0 +1,73 @@
+<template>
+    <chat-window
+            :currentUserId="currentUserId"
+            :rooms="rooms"
+            :messages="messages"
+            :theme="theme"
+            @fetchMessages="fetchMessages"
+    />
+</template>
+
+<script>
+    import ChatWindow from 'vue-advanced-chat'
+    import 'vue-advanced-chat/dist/vue-advanced-chat.css'
+
+    export default {
+        name: "ChatPanel",
+        components: {
+            ChatWindow
+        },
+        data() {
+            return {
+                rooms: [
+                    {
+                        roomId: 1,
+                        roomName: 'Room 1',
+                        unreadCount: 4,
+                        lastMessage: {
+                            content: 'Last message received',
+                            sender_id: 1234,
+                            username: 'John Doe',
+                            timestamp: '10:20',
+                            date: 123242424,
+                            seen: false,
+                            new: true
+                        },
+                        users: [
+                            {
+                                _id: 1234,
+                                username: 'John Doe',
+                                status: {
+                                    state: 'online',
+                                    last_changed: 'today, 14:30'
+                                }
+                            },
+                            {
+                                _id: 4321,
+                                username: 'John Snow',
+                                status: {
+                                    state: 'offline',
+                                    last_changed: '14 July, 20:00'
+                                }
+                            }
+                        ],
+                        typingUsers: [4321]
+                    }
+                ],
+                messages: [],
+                currentUserId: 1234,
+                theme: 'dark'
+            }
+        },
+        methods: {
+            fetchMessages({room, options = {}}) {
+                if (options.reset) this.resetMessages()
+                room;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
